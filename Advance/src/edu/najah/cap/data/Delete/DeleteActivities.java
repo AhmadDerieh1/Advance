568350eb742e4c057f8e40a64502d55f2a1ec07a
AhmadDerieh1 <120324997+AhmadDerieh1@users.noreply.github.com>
2024-01-03 21:15:41 +0200

    ..

---
+++ Advance/src/edu/najah/cap/data/Delete/DeleteActivities.java
@@ -0,0 +1,29 @@
+package edu.najah.cap.data.Delete;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import edu.najah.cap.activity.IUserActivityService;
+import edu.najah.cap.activity.UserActivity;
+import edu.najah.cap.exceptions.BadRequestException;
+import edu.najah.cap.exceptions.NotFoundException;
+import edu.najah.cap.exceptions.SystemBusyException;
+
+public class DeleteActivities implements Deletion {
+    private IUserActivityService userActivityService;
+    public DeleteActivities( IUserActivityService userActivityService) {
+        this.userActivityService = userActivityService;
+    }
+
+    @Override
+    public void removeData(String userName) {
+       try {
+            List<UserActivity> activities = userActivityService.getUserActivity(userName);
+            for (UserActivity activity : new ArrayList<>(activities)) {
+                userActivityService.removeUserActivity(userName, activity.getId());
+            }
+        } catch (SystemBusyException | BadRequestException | NotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
